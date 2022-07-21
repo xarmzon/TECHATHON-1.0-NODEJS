a0f9ef7c9c223d307ff752ada2264b5d06b064bc
unknown <xarmzon@gmail.com>
2022-07-21 09:14:34 +0000

    update to week5

---
+++ week_5/structures/type_based/controllers/users.controllers.js
@@ -0,0 +1,3 @@
+exports.handleDelete = (req,res,next)=>{}
+exports.createAccount = (req,res,next)=>{}
+exports.editAccount = (req,res,next)=>{}
--- week_5/tester/app.js
+++ week_5/tester/app.js
@@ -42,39 +42,43 @@ const app = express();
 //post get put
 
 //todo: router level middleware
-// app.use((req, res, next) => {});
-// const accountRouter = express.Router();
-// accountRouter.use((req, res, next) => {});
-// accountRouter.get("/", (req, res, next) => {});
-// accountRouter.post("/", (req, res, next) => {});
-// accountRouter.put("/", (req, res, next) => {});
-// accountRouter.delete("/", (req, res, next) => {});
+app.use((req, res, next) => {});
 
-// const notesRouter = express.Router();
-// notesRouter.get("/", (req, res, next) => {});
-// notesRouter.post("/", (req, res, next) => {});
-// notesRouter.put("/", (req, res, next) => {});
-// notesRouter.delete("/", (req, res, next) => {});
-
-// app.use("/accounts", accountRouter);
-// app.use("/notes", notesRouter);
+app.use("/accounts", accountRouter);
+app.use("/notes", notesRouter);
 
 //todo: error middleware
-app.get("/users", (req, res) => {
-  // throw "Unknown";
-  res.send("Your messages");
-});
+// app.get("/users", (req, res) => {
+//   // throw "Unknown";
+//   res.send("Your messages");
+// });
 
-// app.use((req,res,next)=>{})
-app.use((err, req, res, next) => {
-  // console.log(err.message)
-  res.status(500).json({ msg: "An error occurred in the app" });
-});
+// // app.use((req,res,next)=>{})
+// app.use((err, req, res, next) => {
+//   // console.log(err.message)
+//   res.status(500).json({ msg: "An error occurred in the app" });
+// });
 
 //todo: built-in
+// express.json
+// express.urlenconded
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// www.g.com
+app.post("/save", (req, res) => {
+  req.file;
+  res.status(201).json({ ...req.body });
+});
 
 //todo: third-party middleware
-
+// multer
+// cors
+// express-validator
+// express-jwt
 //todo: listening for requests
 app.listen(8000, () =>
   console.log(`Server Running on http://localhost:${8000}`)
