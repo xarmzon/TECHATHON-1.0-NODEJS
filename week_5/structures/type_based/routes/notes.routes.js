8e10226fe4ae9fe512493a0ae30d583e680abf9b
unknown <xarmzon@gmail.com>
2022-07-21 18:35:43 +0000

    update to week_6

--- week_5/structures/type_based/routes/notes.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const notesRouter = express.Router();
-notesRouter.get("/", (req, res, next) => {});
-notesRouter.post("/", (req, res, next) => {});
-notesRouter.put("/", (req, res, next) => {});
-notesRouter.delete("/", (req, res, next) => {});
-
-module.exports = notesRouter
--- week_5/structures/type_based/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const { handleDelete, createAccount, editAccount } = require("../controllers/users.controllers");
-
-const accountRouter = express.Router();
-accountRouter.use((req, res, next) => {});
-accountRouter.get("/", (req, res, next) => {});
-accountRouter.post("/", createAccount);
-accountRouter.put("/", editAccount);
-accountRouter.delete("/", handleDelete);
